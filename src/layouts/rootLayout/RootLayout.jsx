6f4211829724e5ee6de6e50fe520797e79223380
Matin Sadeghi <matinsadegi3290@gmail.com>
2025-08-26 16:44:04 +0330

    bug fix

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,14 +1,8 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
 import "./rootLayout.css";
-import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useUserStore } from "../../store/auth";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 const queryClient = new QueryClient();
 
